5570132fc6133b63d7cee32ac8dd9008adeee5d7
mohsen <mohsenaliakbari.atom@hotmail.com>
2026-06-27 15:53:50 +0330

    first commit

---
+++ shared/src/commonMain/kotlin/org/mohsen/reviewtask/Greeting.kt
@@ -0,0 +1,9 @@
+package org.mohsen.reviewtask
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return sayHello(platform.name)
+    }
+}
---
+++ shared/src/commonMain/kotlin/org/mohsen/reviewtask/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package org.mohsen.reviewtask
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
